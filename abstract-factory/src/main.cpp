10b2ad7a021f2b38f3fb2bbd43ab3b701610ab1e
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2025-05-07 19:36:52 +0530

    abstract-factory and builder example added

--- abstract-factory/src/main.cpp
+++ abstract-factory/src/main.cpp
@@ -1,14 +1,67 @@
-//Auto Genrated C++ file by aura CLI
-//None
-#include<iostream>
-#include<design-patternsconfig.h>
-int main(int argc,char*argv[])
+#include <print>
+#include <memory>
+#ifdef _WIN32
+#define GUI WinFactory
+#else
+#define GUI MacFactory
+#endif
+// Abstract products
+class Button
 {
-    std::cerr << "Hello, " << Project::COMPANY_NAME << std::endl;
-    std::cerr << Project::PROJECT_NAME << " v" << Project::VERSION_STRING << std::endl;
-    std::cerr << Project::COPYRIGHT_STRING << std::endl;
-    for(int i=0;i<argc;++i){
-      std::cerr<<argv[i]<<std::endl;
-    }
-    return 0;
+public:
+  virtual void click() = 0;
+};
+class Checkbox
+{
+public:
+  virtual void check() = 0;
+};
+
+// Concrete products
+class WinButton : public Button
+{
+  void click() override { std::print("win : {}\n", __func__); }
+};
+class MacButton : public Button
+{
+  void click() override { std::print("mac : {}\n", __func__); }
+};
+// Concrete products
+class WinCheckbox : public Checkbox
+{
+  void check() override { std::print("win : {}\n", __func__); }
+};
+class MacCheckbox : public Checkbox
+{
+  void check() override { std::print("mac : {}\n", __func__); }
+};
+
+// Abstract factory
+class GUIFactory
+{
+public:
+  virtual Button *createButton() = 0;
+  virtual Checkbox *createCheckbox() = 0;
+};
+
+// Concrete factories
+class WinFactory : public GUIFactory
+{
+public:
+  Button *createButton() override { return new WinButton(); }
+  Checkbox *createCheckbox() override { return new WinCheckbox(); }
+};
+
+class MacFactory : public GUIFactory
+{
+public:
+  Button *createButton() override { return new MacButton(); }
+  Checkbox *createCheckbox() override { return new MacCheckbox(); }
+};
+
+int main()
+{
+  std::unique_ptr<GUIFactory> gui = std::make_unique<GUI>();
+  gui->createButton()->click();
+  gui->createCheckbox()->check();
 }
--- builder/src/main.cpp
+++ builder/src/main.cpp
@@ -1,14 +1,52 @@
-//Auto Genrated C++ file by aura CLI
-//None
-#include<iostream>
-#include<design-patternsconfig.h>
-int main(int argc,char*argv[])
+// Auto Genrated C++ file by aura CLI
+// None
+#include <iostream>
+#include <design-patternsconfig.h>
+#include <print>
+#include <sstream>
+#include <vector>
+#include <string>
+class Splitter
 {
-    std::cerr << "Hello, " << Project::COMPANY_NAME << std::endl;
-    std::cerr << Project::PROJECT_NAME << " v" << Project::VERSION_STRING << std::endl;
-    std::cerr << Project::COPYRIGHT_STRING << std::endl;
-    for(int i=0;i<argc;++i){
-      std::cerr<<argv[i]<<std::endl;
-    }
-    return 0;
+  std::string _input{};
+
+public:
+  explicit Splitter(const std::string &input) : _input(input) {};
+  Splitter() = default;
+  void setInput(const std::string &str) { _input = str; };
+  inline std::string &input() { return _input; };
+  void operator()()
+  {
+    process();
+  }
+  operator std::vector<std::string>()
+  {
+    std::vector<std::string> vec;
+    std::istringstream ss(_input);
+    std::string word{};
+    while (std::getline(ss, word, ' '))
+      vec.emplace_back(word);
+    return vec;
+  };
+
+private:
+  void process()
+  {
+    std::istringstream ss(_input);
+    std::string word{};
+    int index = 0;
+    while (++index, std::getline(ss, word, ' '))
+      std::print("[{}] : {}\n", index, word);
+  };
+};
+int main(int argc, char *argv[])
+{
+  Splitter words;
+  auto &input{words.input()};
+  input = "lol ou th";
+  words();
+  words.setInput("well there are so many things that i don't know so what can i do about it");
+  for (const auto &word : static_cast<std::vector<std::string>>(words))
+    std::print("[{}] ", word);
+  return 0;
 }
